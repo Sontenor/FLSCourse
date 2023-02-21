44b979f33b4cdd8cd03dbf7e2fb57de2a0d42624
Dima_Lobodenko <60902427+Sontenor@users.noreply.github.com>
2023-02-21 19:24:42 +0000

    Add files via upload

---
+++ lesson_9/js/script.js
@@ -0,0 +1,37 @@
+"use strict"
+
+new Swiper('.swiper',{
+
+	navigation: {
+		nextEl:'.swiper-button-next',
+		prevEl:'.swiper-button-prev'
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	mousewheel: {
+		sensitivity: 1,
+		eventsTarget: ".swiper"
+	},
+	autoHeight: false,
+	slidesPerView: 4,
+	spaceBetween: 30,
+	loop: true,
+});
+
+
+const iconMenu = document.querySelector('.menu__vector');
+if (iconMenu) {
+	const menuBody = document.querySelector('.sub__menu');
+	iconMenu.addEventListener("click", function (e) {
+		iconMenu.classList.toggle('active');
+		menuBody.classList.toggle('active');
+	});
+};
+
+
+
+
+
+
